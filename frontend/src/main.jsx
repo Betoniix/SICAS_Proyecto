345075590d65158f5db48c4c24f1ce0eceaa138e
Iván Cetina <ivanjceu@gmail.com>
2023-11-11 17:56:46 -0600

    vistas
    Aforo y cuestionario admin

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Aforo from "./modules/aforo"
+import Cuestionario from "./modules/cuestionario"
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <Aforo></Aforo>
+    <Cuestionario></Cuestionario>
+    
+  </React.StrictMode>
 );
---
+++ frontend/src/modules/aforo.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "../index.css";
+import logo from "../assets/escudo.png";
+
+function aforo(){
+    
+    return(
+        <>
+        <div className="bg-[#0A1446] h-24 grid grid-cols-3 justify-items-center content-center">
+            <img src={logo} alt="" className="w-10"/>
+            <div className="text-white text-2xl self-center">Sistema de Control de Asistencia Segura</div>
+            <div className="">
+            <button className="h-10 bg-[#D50F0F] rounded-md text-white w-28 ">Cerrar Sesión</button>
+            </div>
+        </div>
+        <div className=" grid grid-cols-3 content-center h-4/5 mt-10">
+            <div className="bg-white justify-self-center border-black border-2 h-[550px] w-[400px] grid grid-cols-1">
+                <a className="bg-[#0A1446] w-44 h-14 rounded-md text-center self-center justify-self-center py-4 text-white">Alertas</a>
+                <a className="bg-[#0A1446] w-44 h-14 rounded-md text-center self-center justify-self-center py-4 text-white">Aforo</a>
+                <a className="bg-[#0A1446] w-44 h-14 rounded-md text-center self-center justify-self-center py-4 text-white">Aulas</a>
+                <a className="bg-[#0A1446] w-44 h-14 rounded-md text-center self-center justify-self-center py-4 text-white">Cuestionario</a>
+            </div>
+            
+            <div className="col-span-2 bg-[#E8E8E8] justify-self-center h-[550px] w-full mr-12 grid grid-cols-1">
+                <div className="self-center justify-self-center text-2xl font-bold">Aforo</div>
+                <div className="flex flex-col justify-self-center text-center self-center">
+                    Porcentaje de capacidad
+                    <input className="w-60 h-8 rounded-md border-black border-2 p-1"></input>
+                </div>
+                <div className="bg-[#D5A00F] w-44 h-14 rounded-md text-center self-center justify-self-center py-4 ">Actualizar</div>
+
+            </div>
+        </div>
+        </>
+        );
+
+}
+
+export default aforo;
