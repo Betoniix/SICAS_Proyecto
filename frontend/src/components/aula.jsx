42ccbc5a3003cb0620c8ed0acce56b7bd6a7364e
MCAlberto55 <alberto99emc@outlook.com>
2023-11-11 18:09:31 -0600

    vistas de edicion y agregar aulas

---
+++ frontend/src/components/aula.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import AulasModal from "./aulasModal";
+
+function Aula({ Nombre, Edificio, Capacidad }) {
+  const [mostrarForm, setMostrarForm] = useState(false);
+  const [nombre, setNombre] = useState(Nombre);
+  const [edificio, setEdificio] = useState(Edificio);
+  const [capacidad, setCapacidad] = useState(Capacidad);
+
+  const editarOnSubmit = (nombre, edificio, capacidad) => {
+    setNombre(nombre);
+    setCapacidad(capacidad);
+    setEdificio(edificio);
+    setMostrarForm(false);
+  };
+
+  const actualizarAula = () => {
+    //backend
+    setMostrarForm(false);
+  };
+
+  const eliminar = () => {};
+
+  const editar = () => {
+    setMostrarForm(true);
+  };
+
+  return (
+    <div class="w-[100%] flex justify-evenly my-8">
+      {mostrarForm && (
+        <AulasModal
+          editarOnSubmit={editarOnSubmit}
+          nombre={nombre}
+          setNombre={setNombre}
+          edificio={edificio}
+          setEdificio={setEdificio}
+          capacidad={capacidad}
+          setCapacidad={setCapacidad}
+        ></AulasModal>
+      )}
+
+      <p class="text-[30px]">
+        [{nombre}] - [{edificio}] - [{capacidad}]
+      </p>
+      <button
+        class="w-[214px] h-[60px] bg-[#D5A00F] text-white text-[25px] font-normal rounded-[10px]"
+        onClick={() => editar()}
+      >
+        Editar
+      </button>
+      <button
+        class="w-[214px] h-[60px] bg-[#D50F0F] text-white text-[25px] font-normal rounded-[10px]"
+        onClick={() => eliminar()}
+      >
+        Eliminar
+      </button>
+      {mostrarForm ? true : false}
+    </div>
+  );
+}
+
+export default Aula;
--- frontend/src/modules/gestionAulas.jsx
+++ frontend/src/modules/gestionAulas.jsx
@@ -1,96 +1,111 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import Header from "../components/header";
 import NavBar from "../components/navBar";
+import AulasModal from "../components/aulasModal";
+import Aula from "../components/aula";
+import AniadirModal from "../components/aniadirModal";
 
-function GestionAulas(){
+function GestionAulas() {
+  let AulasActuales = [
+    {
+      NombreAula: "DB-02",
+      Edificio: "Edificio D",
+      Capacidad: "26",
+    },
+    {
+      NombreAula: "EE-03",
+      Edificio: "Edificio A",
+      Capacidad: "45",
+    },
+    {
+      NombreAula: "DB-04",
+      Edificio: "Edificio D",
+      Capacidad: "23",
+    },
+    {
+      NombreAula: "EE-01",
+      Edificio: "Edificio A",
+      Capacidad: "50",
+    },
+    {
+      NombreAula: "HA-02",
+      Edificio: "Edifcio H",
+      Capacidad: "30",
+    },
+    {
+      NombreAula: "HA-02",
+      Edificio: "Edifcio H",
+      Capacidad: "30",
+    },
+    {
+      NombreAula: "HA-02",
+      Edificio: "Edifcio H",
+      Capacidad: "30",
+    },
+    {
+      NombreAula: "HA-02",
+      Edificio: "Edifcio H",
+      Capacidad: "30",
+    },
+  ];
 
-    let AulasActuales = [
-        {
-            "NombreAula": "DB-02",
-            "Edificio": "Edificio D",
-            "Capacidad": "26"
-        },
-        {
-            "NombreAula": "EE-03",
-            "Edificio": "Edificio A",
-            "Capacidad": "45"
-        },
-        {
-          "NombreAula": "DB-04",
-          "Edificio": "Edificio D",
-          "Capacidad": "23"
-        },
-        {
-          "NombreAula": "EE-01",
-          "Edificio": "Edificio A",
-          "Capacidad": "50"
-        },
-        {
-          "NombreAula": "HA-02",
-          "Edificio": "Edifcio H",
-          "Capacidad": "30"
-        },
-        {
-            "NombreAula": "HA-02",
-            "Edificio": "Edifcio H",
-            "Capacidad": "30"
-        },
-        {
-            "NombreAula": "HA-02",
-            "Edificio": "Edifcio H",
-            "Capacidad": "30"
-        },
-        {
-            "NombreAula": "HA-02",
-            "Edificio": "Edifcio H",
-            "Capacidad": "30"
-        },
-          
-      ];
-    
-    const [aulas, setAulas] = useState(AulasActuales);
+  const [aulas, setAulas] = useState(AulasActuales);
+  const [mostrarForm, setMostrarForm] = useState(false);
+  const [formData, setFormData] = useState({});
 
-    function restringir() {
+  function anadirOnSubmit(aula, edificio, capacidad) {
+    let copyAulas = aulas;
 
-    }
+    const nuevaAula = {
+      NombreAula: aula,
+      Edificio: edificio,
+      Capacidad: capacidad,
+    };
 
-    function editar() {
-        //modificar con el index, simular peticion ya ctualizar. USAR UNA COPIA
-    }
+    copyAulas.push(nuevaAula);
 
-    function eliminar() {
-        //quitar con el index, simular peticion y actualizar. USAR UNA COPIA
-    }
+    setMostrarForm(false);
+    setAulas(copyAulas);
+  }
 
-    return(
-        <>
-        <Header></Header>
-        <div class="h-[85vh] flex justify-evenly items-center justify-items-center justify-self-center">
-            <NavBar/>
-            <div class="h-[100%] flex flex-col justify-around items-center">
-                <div class="w-[1338px] h-[686px] max-h-[686px] bg-[#D9D9D9] flex flex-col content-center items-center overflow-auto ">
-                    <p class="font-bold text-[30px] ">Aulas</p>
-                    {aulas.map(aula => 
-                        <>
-                            <div class="w-[100%] flex justify-evenly my-8">
-                                <p class="text-[30px]">[{aula.NombreAula}] - [{aula.Edificio}] - [{aula.Capacidad}]</p>
-                                <button class="w-[214px] h-[60px] bg-[#D5A00F] text-white text-[25px] font-normal rounded-[10px]" 
-                                    onClick={() => editar()}>Editar
-                                </button>
-                                <button class="w-[214px] h-[60px] bg-[#D50F0F] text-white text-[25px] font-normal rounded-[10px]" 
-                                    onClick={() => eliminar()}>Eliminar
-                                </button>
-                            </div>
-                        </> 
-                    )}
-                </div>
-                <button class="w-[214px] h-[60px] bg-[#D5A00F] text-white text-[25px] font-normal rounded-[10px]" 
-                    onClick={() => restringir()}>Añadir
-                </button>
-            </div>
+  function añadir() {
+    setMostrarForm(true);
+  }
+
+  function eliminar() {
+    //quitar con el index, simular peticion y actualizar. USAR UNA COPIA
+  }
+
+  return (
+    <>
+      <Header></Header>
+      {mostrarForm && (
+        <AniadirModal anadirOnSubmit={anadirOnSubmit}></AniadirModal>
+      )}
+      <div class="h-[85vh] flex justify-evenly items-center justify-items-center justify-self-center">
+        <NavBar />
+        <div class="h-[100%] flex flex-col justify-around items-center">
+          <div class="w-[1338px] h-[686px] max-h-[686px] bg-[#D9D9D9] flex flex-col content-center items-center overflow-auto ">
+            <p class="font-bold text-[30px] ">Aulas</p>
+            {aulas.map((aula) => (
+              <Aula
+                Nombre={aula.NombreAula}
+                Edificio={aula.Edificio}
+                Capacidad={aula.Capacidad}
+              ></Aula>
+            ))}
+          </div>
+          <button
+            class="w-[214px] h-[60px] bg-[#D5A00F] text-white text-[25px] font-normal rounded-[10px]"
+            onClick={() => añadir()}
+          >
+            Añadir
+          </button>
+          {mostrarForm ? true : false}
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default GestionAulas;
